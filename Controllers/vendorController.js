2fd659b3e0b889d62f9799abd05f91a698058be5
sujith <sujisurendran10101@gmail.com>
2024-02-03 12:22:51 +0530

    vendor login updated

--- Controllers/vendorController.js
+++ Controllers/vendorController.js
@@ -72,7 +72,7 @@ export const vendorSignin = async (req,res,next)=>{
         const isPasswordCorrect= await bcrypt.compare(password,vendor.password);
        
         if(isPasswordCorrect){
-          // if(vendor.isAccess=="Allowed"){
+     
 
           
             // console.log("password matched");
@@ -82,10 +82,7 @@ export const vendorSignin = async (req,res,next)=>{
            
             const expiryDate = new Date(Date.now()+3600000)
             return res.status(200).json({rest,message:"vendor login successful",token})
-        // }
-        // else{
-        //   return res.status(401).json({message:"user blocked"})
-        // }
+        
            }else{
             // next(errorHandler(401,"Invalid password"))
             return res.status(403).json({message:"invalid credentials"})
